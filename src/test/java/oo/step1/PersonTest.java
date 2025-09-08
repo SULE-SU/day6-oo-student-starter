550c4b01192a380027988dcdd3ab7e15aa760c29
SULE <sule1@oocl.com>
2025-09-08 10:28:57 +0800

    test: add should_return_a_message_with_name_and_age_when_introduce unitTest

--- src/test/java/oo/step1/PersonTest.java
+++ src/test/java/oo/step1/PersonTest.java
@@ -1,12 +1,21 @@
 package oo.step1;
 
+import oo.Person;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PersonTest {
 
 
     // when introduce a person, then return a message with name and age
+    @Test
+    public void should_return_a_message_with_name_and_age_when_introduce(){
+        Person person = new Person(1,"Tom",20);
+        assertEquals("My name is Tom. I am 20 years old.",person.introduce());
+    }
 
-    // when equals two same id person then return true
 
-    // when equals two different id person then return true
+
 
 }
